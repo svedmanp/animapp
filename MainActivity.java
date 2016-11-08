bee071f499bee4f4d0c20efcee1dad6f184c839d
hffmiklin <hannah.miklin@gmail.com>
2016-11-08 12:01:12 +0100

    Wrote a comment again

--- MainActivity.java
+++ MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     /* hello pontus-san */
+    /* en till kommentar */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
